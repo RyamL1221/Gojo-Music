0b79164ddecfbce9518b9b1ae31baca19c0e1705
RyamL1221 <ryaml1221@gmail.com>
2024-10-10 15:04:02 -0400

    Added inputs, login, and starting the bot

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-const { Client, GuildMember, Intents } = require('discord.js');
-const { Player, QueryType } = require('discord-player');
+// Import the discord.js module
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
+const { Player } = require('discord-player');
 const config = require('./config.json');
+
+// create client and login
+const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildVoiceStates,
+    ]
+  });
+client.login(config.token);
+
+// Add listeners for startup
+client.once('ready', () => {
+    console.log('Ready!');
+});
+client.on("error", console.error);
+client.on("warn", console.warn);
+
